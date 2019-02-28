c45acdc6e0888fa57f27a539685be7cf29124d4b
Brandon O'Connor <brandonjacob@google.com>
2019-02-28 11:31:09 -0800

    pulled upstream and fmt

--- variables.tf
+++ variables.tf
@@ -222,6 +222,7 @@ variable "iam_disable_polling" {
   description = "Whether to disable polling for IAM API"
   default     = "False"
 }
+
 variable "logging_max_calls" {
   description = "Maximum calls that can be made to Logging API"
   default     = "9"
@@ -236,6 +237,7 @@ variable "logging_disable_polling" {
   description = "Whether to disable polling for Logging API"
   default     = "False"
 }
+
 variable "securitycenter_max_calls" {
   description = "Maximum calls that can be made to Security Center API"
   default     = "1"
